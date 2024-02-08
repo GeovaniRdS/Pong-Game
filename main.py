849c4b90aae3a1363856b68416b4c713aeb5b44c
Geovani <geovanir.ds@gmail.com>
2024-02-08 14:55:23 -0300

    Setuping Screen

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from turtle import Screen
-from screen import screen_setup
+from turtle import Turtle, Screen
 
-def main():
-    new_screen = screen_setup
-    new_screen
+# Screnn
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
 
 
-main()
 
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() +20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+
+screen.exitonclick()
--- screen.py
+++
@@ -1,9 +0,0 @@
-from turtle import Screen
-
-class screen_setup():
-    new_screen = Screen()
-    
-    new_screen.bgcolor("black")
-    new_screen.setup(width=1100, height=600)
-
-    new_screen.exitonclick()
