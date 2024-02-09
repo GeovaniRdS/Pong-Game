e2c95a256d1a991c35a5bef39303579417585544
Geovani <geovanir.ds@gmail.com>
2024-02-09 15:35:54 -0300

    The last Test

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
-
-
+screen.tracer(0)
 
 
 paddle = Turtle()
@@ -18,10 +17,24 @@ paddle.goto(350, 0)
 
 
 def go_up():
-    new_y = paddle.ycor() +20
+    new_y = paddle.ycor() + 20
     paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
 
 screen.listen()
 screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+game_is_on = True
+
+while game_is_on == True:
+    screen.update()
+
 
 screen.exitonclick()
